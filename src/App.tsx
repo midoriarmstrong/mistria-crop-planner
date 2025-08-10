729455cb3d0bca82e64d9b499773c9a7e7ddeac5
Midori Armstrong <midoriarmstrong@outlook.com>
2025-08-10 00:29:04 -0700

    Add basic Fields of Mistria theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/logo.png';
+import Calendar from './calendar/Calendar';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>
+          <img src={logo} alt="Fields of Mistria" />
+          Crop Planner
+        </h1>
+      </header>
+      <main>
+        <Calendar />
+      </main>
+      <footer>
+        <a href="https://www.fieldsofmistria.com/" target="_blank" rel="noreferrer">Fields of Mistria</a> (v0.14.1) © NPC Studio.
+        Unofficial <a href="https://github.com/midoriarmstrong/mistria-crop-planner" target="_blank" rel="noreferrer">crop planner source code</a> made available under the MIT License.
+        Design based on <a href="https://github.com/exnil" target="_blank" rel="noreferrer">exnil</a>'s <a href="https://exnil.github.io/crop_planner/" target="_blank" rel="noreferrer">Stardew Valley crop planner</a>.
+      </footer>
     </>
   )
 }
-
-export default App
---
+++ src/calendar/Calendar.tsx
@@ -0,0 +1,12 @@
+export default function Calendar() {
+  return (
+    <div className="calendar">
+      <div className="calendar-header">
+
+      </div>
+      <div className="calendar-months">
+
+      </div>
+    </div>
+  )
+}
